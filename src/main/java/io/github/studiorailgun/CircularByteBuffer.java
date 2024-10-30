c06a00cf726815d3fab558673c29913a70b3fdf9
AustinHoover <25714838+AustinHoover@users.noreply.github.com>
2024-10-30 16:01:46 -0400

    Allow CircularByteBuffer to infinitely overflow

--- src/main/java/io/github/studiorailgun/CircularByteBuffer.java
+++ src/main/java/io/github/studiorailgun/CircularByteBuffer.java
@@ -1,5 +1,9 @@
 package io.github.studiorailgun;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -8,6 +12,9 @@ import java.util.concurrent.Semaphore;
  * </p>
  * <p>
  * When the sizes of the reads are small enough, the buffer will not perform any reallocations.
+ * If the backing array overflows, it will fall back to using a queue to store blocks of bytes until it can recover.
+ * The backing array will not be re-filled until the queue has fully cleared.
+ * This means reads/writes will be slow until the queue has fully cleared.
  * </p>
  * <p>
  * The buffer is threadsafe.
@@ -33,7 +40,7 @@ public class CircularByteBuffer {
     /**
      * The capacity of the backing array
      */
-    int capacity;
+    int primaryCapacity;
     
     /**
      * Lock to make the structure threadsafe
@@ -41,14 +48,24 @@ public class CircularByteBuffer {
     Semaphore lock = new Semaphore(1);
 
     /**
-     * Constructs a CircularByteBuffer
-     * @param capacity The capacity of the backing array in bytes
+     * Stores bytes when the array is overloaded
      */
-    public CircularByteBuffer(int capacity){
-        backingArray = new byte[capacity];
+    Deque<byte[]> overflowQueue = new LinkedBlockingDeque<byte[]>();
+
+    /**
+     * Used for cleaning up the overflowQueue after a read
+     */
+    List<byte[]> clearQueue = new LinkedList<byte[]>();
+
+    /**
+     * Constructs a CircularByteBuffer
+     * @param primaryCapacity The capacity of the backing array in bytes
+     */
+    public CircularByteBuffer(int primaryCapacity){
+        backingArray = new byte[primaryCapacity];
         position = 0;
         remaining = 0;
-        this.capacity = capacity;
+        this.primaryCapacity = primaryCapacity;
     }
 
     /**
@@ -58,18 +75,25 @@ public class CircularByteBuffer {
      */
     public void add(byte[] bytes, int len){
         lock.acquireUninterruptibly();
-        // System.out.println("Add start");
-        int writePosition = (position + remaining) % capacity;
-        //amount possible to write before wrapping
-        int writeBeforeWrap = capacity - writePosition;
-        //only run wrapping logic if necessary
-        if(len > writeBeforeWrap){
-            System.arraycopy(bytes, 0, backingArray, writePosition, writeBeforeWrap);
-            System.arraycopy(bytes, writeBeforeWrap, backingArray, 0, len - writeBeforeWrap);
+
+        //if adding directly to array would overload, put into overflow queue instead
+        if(overflowQueue.size() > 0 || primaryCapacity - remaining < len){
+            byte[] storage = new byte[len];
+            System.arraycopy(bytes, 0, storage, 0, len);
+            overflowQueue.add(storage);
         } else {
-            System.arraycopy(bytes, 0, backingArray, writePosition, len);
+            int writePosition = (position + remaining) % primaryCapacity;
+            //amount possible to write before wrapping
+            int writeBeforeWrap = primaryCapacity - writePosition;
+            //only run wrapping logic if necessary
+            if(len > writeBeforeWrap){
+                System.arraycopy(bytes, 0, backingArray, writePosition, writeBeforeWrap);
+                System.arraycopy(bytes, writeBeforeWrap, backingArray, 0, len - writeBeforeWrap);
+            } else {
+                System.arraycopy(bytes, 0, backingArray, writePosition, len);
+            }
+            remaining = remaining + len;
         }
-        remaining = remaining + len;
         lock.release();
     }
 
@@ -89,7 +113,27 @@ public class CircularByteBuffer {
      */
     public byte peek(int offset){
         lock.acquireUninterruptibly();
-        byte rVal = backingArray[(position + offset) % capacity];
+        byte rVal = -1;
+        if(offset < remaining){
+            rVal = backingArray[(position + offset) % primaryCapacity];
+        } else if(overflowQueue.size() > 0){
+            int accumulatedOffset = remaining;
+            byte[] readTarget = null;
+            //search through the overflow queue until we've found the array to offset into
+            for(byte[] storage : overflowQueue){
+                if(storage.length + accumulatedOffset > offset){
+                    readTarget = storage;
+                    break;
+                } else {
+                    accumulatedOffset = accumulatedOffset + storage.length;
+                }
+            }
+
+            if(readTarget != null){
+                int relativePosition = offset - accumulatedOffset;
+                rVal = readTarget[relativePosition];
+            }
+        }
         lock.release();
         return rVal;
     }
@@ -101,17 +145,22 @@ public class CircularByteBuffer {
     public int getRemaining(){
         lock.acquireUninterruptibly();
         int rVal = remaining;
+        if(overflowQueue.size() > 0){
+            for(byte[] storage : overflowQueue){
+                rVal = rVal + storage.length;
+            }
+        }
         lock.release();
         return rVal;
     }
 
     /**
-     * Gets the capacity of the buffer
+     * Gets the capacity of the primary buffer
      * @return The capacity
      */
     public int getCapacity(){
         lock.acquireUninterruptibly();
-        int rVal = capacity;
+        int rVal = primaryCapacity;
         lock.release();
         return rVal;
     }
@@ -124,16 +173,63 @@ public class CircularByteBuffer {
     public byte[] read(int len){
         lock.acquireUninterruptibly();
         byte[] rVal = new byte[len];
-        //amount possible to read before loop
-        int toReadBeforeLoop = capacity - position;
-        if(len > capacity - position){
-            System.arraycopy(backingArray, position, rVal, 0, toReadBeforeLoop);
-            System.arraycopy(backingArray, 0, rVal, toReadBeforeLoop, len - toReadBeforeLoop);
+        if(len > remaining && overflowQueue.size() > 0){
+            //read from primary array
+            //amount possible to read before loop
+            int toReadBeforeLoop = primaryCapacity - position;
+            if(remaining > primaryCapacity - position){
+                System.arraycopy(backingArray, position, rVal, 0, toReadBeforeLoop);
+                System.arraycopy(backingArray, 0, rVal, toReadBeforeLoop, remaining - toReadBeforeLoop);
+            } else {
+                System.arraycopy(backingArray, position, rVal, 0, remaining);
+            }
+            
+            //read from overflow queues
+            int posToWriteTo = remaining;
+            int remainingToRead = len - remaining;
+            clearQueue.clear();
+            for(byte[] storage : overflowQueue){
+                if(remainingToRead == 0){
+                    break;
+                }
+                if(storage.length > remainingToRead){
+                    System.arraycopy(storage, 0, rVal, posToWriteTo, remainingToRead);
+                    //reallocate remaining bytes
+                    byte[] realloc = new byte[storage.length - remainingToRead];
+                    System.arraycopy(storage, remainingToRead, realloc, 0, storage.length - remainingToRead);
+                    overflowQueue.addFirst(realloc);
+
+                    clearQueue.add(storage);
+                    posToWriteTo = -1;
+                    remainingToRead = 0;
+                    break;
+
+                } else {
+                    System.arraycopy(storage, 0, rVal, posToWriteTo, storage.length);
+                    posToWriteTo = posToWriteTo + storage.length;
+                    remainingToRead = remainingToRead - storage.length;
+                    clearQueue.add(storage);
+                }
+            }
+            //remove from overflow queue
+            overflowQueue.removeAll(clearQueue);
+
+            //update tracked values
+            position = (position + remaining) % primaryCapacity;
+            remaining = 0;
+
         } else {
-            System.arraycopy(backingArray, position, rVal, 0, len);
+            //amount possible to read before loop
+            int toReadBeforeLoop = primaryCapacity - position;
+            if(len > primaryCapacity - position){
+                System.arraycopy(backingArray, position, rVal, 0, toReadBeforeLoop);
+                System.arraycopy(backingArray, 0, rVal, toReadBeforeLoop, len - toReadBeforeLoop);
+            } else {
+                System.arraycopy(backingArray, position, rVal, 0, len);
+            }
+            position = (position + len) % primaryCapacity;
+            remaining = remaining - len;
         }
-        position = (position + len) % capacity;
-        remaining = remaining - len;
         lock.release();
         return rVal;
     }
--- src/test/java/io/github/studiorailgun/CircularByteBufferTests.java
+++ src/test/java/io/github/studiorailgun/CircularByteBufferTests.java
@@ -1,6 +1,7 @@
 package io.github.studiorailgun;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -29,10 +30,9 @@ public class CircularByteBufferTests {
         CircularByteBuffer buffer = new CircularByteBuffer(5);
 
         //write to buffer
-        byte[] input = new byte[]{
+        buffer.add(new byte[]{
             1,2,3,4,5
-        };
-        buffer.add(input, 5);
+        }, 5);
 
         //read from buffer
         byte[] output = buffer.read(5);
@@ -54,19 +54,17 @@ public class CircularByteBufferTests {
         CircularByteBuffer buffer = new CircularByteBuffer(5);
 
         //write to buffer
-        byte[] input1 = new byte[]{
+        buffer.add(new byte[]{
             1,2,3,4,5
-        };
-        buffer.add(input1, 5);
+        }, 5);
 
         //read from buffer
         byte[] output1 = buffer.read(3);
 
         //write again
-        byte[] input2 = new byte[]{
+        buffer.add(new byte[]{
             6,7,8
-        };
-        buffer.add(input2, 3);
+        }, 3);
 
         //read again
         byte[] output2 = buffer.read(5);
@@ -83,6 +81,306 @@ public class CircularByteBufferTests {
         assertEquals(output2[2], 6);
         assertEquals(output2[3], 7);
         assertEquals(output2[4], 8);
+    }
+
+    /**
+     * Test that data can be written to and read from the buffer without issue when the buffer wraps around on itself
+     */
+    @Test
+    public void testReadWithOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        byte[] output1 = buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        assertEquals(7,buffer.getRemaining());
+        byte[] output2 = buffer.read(7);
+
+        //assert data is correct
+        assertEquals(output1.length, 3);
+        assertEquals(output1[0], 1);
+        assertEquals(output1[1], 2);
+        assertEquals(output1[2], 3);
+
+        assertEquals(output2.length, 7);
+        assertEquals(output2[0], 4);
+        assertEquals(output2[1], 5);
+        assertEquals(output2[2], 6);
+        assertEquals(output2[3], 7);
+        assertEquals(output2[4], 8);
+        assertEquals(output2[5], 9);
+        assertEquals(output2[6], 10);
+    }
+
+    /**
+     * Test that remaining value is accurate once an overflow happens
+     */
+    @Test
+    public void testRemainingDuringOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        assertEquals(7,buffer.getRemaining());
+
+        //assert data is correct
+        assertEquals(buffer.getRemaining(), 7);
+    }
+
+    /**
+     * Test that remaining value is accurate after an overflow
+     */
+    @Test
+    public void testRemainingAfterOverflowClears(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.read(7);
+
+        //make sure remaining is accurate
+        assertEquals(0,buffer.getRemaining());
+    }
+
+    /**
+     * Test that read/write still works with small blocks after an overflow clears
+     */
+    @Test
+    public void testReadAfterOverflowClears(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.read(7);
+
+        //read and write bytes
+        buffer.add(new byte[]{
+            47,17
+        },2);
+        byte[] output = buffer.read(2);
+
+        //assert values are accurate
+        assertEquals(47, output[0]);
+        assertEquals(17, output[1]);
+    }
+
+    /**
+     * Test that we can read both blocks if the buffer is written two after it has already overflowed
+     */
+    @Test
+    public void testWritingTwoOverflowBlocks(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        //read and write bytes
+        byte[] output = buffer.read(14);
+
+        //assert values are accurate
+        assertEquals(4, output[0]);
+        assertEquals(5, output[1]);
+        assertEquals(6, output[2]);
+        assertEquals(7, output[3]);
+        assertEquals(8, output[4]);
+        assertEquals(9, output[5]);
+        assertEquals(10, output[6]);
+        assertEquals(11, output[7]);
+        assertEquals(12, output[8]);
+        assertEquals(13, output[9]);
+    }
+
+    /**
+     * Test that the data is correct if we read and it doesn't fully clear the overflow
+     */
+    @Test
+    public void testNotFullyClearingOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        //read and write bytes
+        byte[] output = buffer.read(12);
+
+        //assert values are accurate
+        assertEquals(4, output[0]);
+        assertEquals(5, output[1]);
+        assertEquals(6, output[2]);
+        assertEquals(7, output[3]);
+        assertEquals(8, output[4]);
+        assertEquals(9, output[5]);
+        assertEquals(10, output[6]);
+        assertEquals(11, output[7]);
+    }
+
+    /**
+     * Test that the partial data is preserved in the queue after not fully clearing the overflow
+     */
+    @Test
+    public void testNotFullyClearingOverflowDataSecurity(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        //read and write bytes
+        buffer.read(12);
+        byte[] output = buffer.read(2);
+
+        //assert values are accurate
+        assertEquals(16, output[0]);
+    }
+
+    /**
+     * Test that the queue can add new non-overflowing blocks after not fully clearing an overflow
+     */
+    @Test
+    public void testAddNonOverflowBlockAfterNotFullyClearingOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        //read and write bytes
+        buffer.read(12);
+        buffer.add(new byte[]{
+            1,2
+        },2);
+        buffer.add(new byte[]{
+            3,4
+        },2);
+        byte[] output = buffer.read(5);
+
+        //assert values are accurate
+        assertEquals(16, output[0]);
+        assertEquals(1, output[1]);
+        assertEquals(2, output[2]);
+        assertEquals(3, output[3]);
+        assertEquals(4, output[4]);
+    }
+
+    /**
+     * Test that the queue can add new overflowing blocks after not fully clearing an overflow
+     */
+    @Test
+    public void testAddOverflowBlockAfterNotFullyClearingOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        //read and write bytes
+        buffer.read(12);
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        },5);
+        byte[] output = buffer.read(3);
+
+        //assert values are accurate
+        assertEquals(16, output[0]);
+        assertEquals(1, output[1]);
+        assertEquals(2, output[2]);
+    }
+
+    /**
+     * Test peek() accuracy during overflow
+     */
+    @Test
+    public void testPeekDuringOverflow(){
+        CircularByteBuffer buffer = new CircularByteBuffer(5);
+
+        //cause overflow and cleanup overflow
+        buffer.add(new byte[]{
+            1,2,3,4,5
+        }, 5);
+        buffer.read(3);
+        buffer.add(new byte[]{
+            6,7,8,9,10
+        }, 5);
+        buffer.add(new byte[]{
+            11,12,13,14,15,16
+        }, 6);
+
+        
+        assertEquals(4,buffer.peek());
+        assertEquals(6,buffer.peek(2));
+        assertEquals(10,buffer.peek(6));
+        assertEquals(11,buffer.peek(7));
+        assertEquals(15,buffer.peek(11));
     }
 
 }
